b84cb3c5b41adf766699b25e25a9c211f859fb28
DmitriySh <dashishmakov@gmail.com>
2018-04-18 14:34:06 +0300

    Delete spaces in item

--- fill_test_collection.js
+++ fill_test_collection.js
@@ -5,7 +5,7 @@ db.getSiblingDB('test').test_collection.createIndex({'number' : 1});
 sh.shardCollection('test.test_collection', {'number':1});
 
 var bulk = db.getSiblingDB('test').test_collection.initializeUnorderedBulkOp();
-people = ['Marc', 'Bill', 'George', 'Eliot', 'Matt', 'Trey', 'Tracy', 'Greg', 'Steve', 'Kristina', 'Katie', '    Jeff'];
+people = ['Marc', 'Bill', 'George', 'Eliot', 'Matt', 'Trey', 'Tracy', 'Greg', 'Steve', 'Kristina', 'Katie', 'Jeff'];
 for(var i = 0; i < 1000000; i++) {
     user_id = i;
     name = people[Math.floor(Math.random() * people.length)];
